fe8cf63b3ce8f557e05fae05b96029692c50809f
Muhammad Faiq Mubarok <faiqmubarok@gmail.com>
2025-05-02 03:17:32 +0700

    feat: create products services and connect to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-const app = express();
-
 dotenv.config();
 
-const PORT = process.env.PORT;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.send("Selamat datang di API akuh");
+  res.send("Selamat datang di Backend Project API!");
 });
 
 const productController = require("./product/product.controller");
-
 app.use("/products", productController);
 
 app.listen(PORT, () => {
-  console.log("Express API running in port: " + PORT);
+  console.log(`Express API running on port: ${PORT}`);
 });
